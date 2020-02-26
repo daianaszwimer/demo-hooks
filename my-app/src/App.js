20334e75a94bb809e67010274ce57889cde03e35
Daiana Szwimer <daiana.szwimer@prosoftwarefactory.com.ar>
2020-02-25 22:10:43 -0300

    use effect eg

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -73,3 +73,32 @@ function App() {
 
 export default App;
 */
+
+
+/* useEffect
+import React, { useState } from 'react';
+
+function App() {
+
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0); // 0 is the default value
+  // the state is not an object
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+		<div>
+			<p>You clicked {count} times</p>
+			<button onClick={() => setCount(count + 1)}>
+				Click me
+			</button>
+		</div>
+	)
+}
+
+export default App;
+* */
